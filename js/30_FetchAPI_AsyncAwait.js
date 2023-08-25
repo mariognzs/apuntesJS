49dba64821eda9db0583921ca362ae19fb15d8bd
mariognzs <mariogonzalezsolas@gmail.com>
2023-08-25 15:21:43 +0200

    Apuntes JS

---
+++ js/30_FetchAPI_AsyncAwait.js
@@ -0,0 +1,12 @@
+// Fetch API  - Async Await
+
+const url = "https://jsonplaceholder.typicode.com/comments"
+
+const consultarAPI = async () => {
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+    // resultado.forEach( comentario => {
+    //     console.log(comentario)
+    // })
+}
+consultarAPI();
